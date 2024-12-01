c63227d2b8a3b401af838f38868ee125d3022523
Gentes720 <lontsiangelin@gmail.com>
2024-12-01 02:02:14 +0100

    Exercise 29 distance between prime numbers

---
+++ 029.py
@@ -0,0 +1,35 @@
+
+import numpy as np
+
+def prime(n):
+    if n < 1:
+        return False
+    if n == 1:
+        return True
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    max_divisor = int(np.sqrt(n)) + 1
+    for i in range(3, max_divisor, 2):
+        if n % i == 0:
+            return False
+    return True
+
+# creation of an array of 100 element
+array = np.zeros(100, dtype=int)
+
+# Research of the 100 first prime numbers
+count = 0
+num = 1
+while count < 100:
+    if prime(num):
+        array[count] = num
+        count += 1
+    num += 1
+
+# results
+distances = []
+for i in range(len(array)- 1):
+    distances.append(int(array[i +1]-array[i]))
+print(distances)
