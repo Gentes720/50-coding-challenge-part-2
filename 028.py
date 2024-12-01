ef12f24eb518ab443446bdb3036119f17fc18cdc
Gentes720 <lontsiangelin@gmail.com>
2024-12-01 02:00:59 +0100

    Exercise 28 sum of prime numbers

---
+++ 028.py
@@ -0,0 +1,33 @@
+
+import numpy as np
+
+def prime(n):
+    if n < 1:
+        return False
+    if n == 1:
+        return True
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    max_divisor = int(np.sqrt(n)) + 1
+    for i in range(3, max_divisor, 2):
+        if n % i == 0:
+            return False
+    return True
+
+# creation of an array of 100 element
+array = np.zeros(100, dtype=int)
+
+# Research of the 100 first prime numbers
+count = 0
+num = 1
+while count < 100:
+    if prime(num):
+        array[count] = num
+        count += 1
+    num += 1
+
+# results
+print(" the sum of the first 100 prime numbers is", array.sum())
+print(array)
