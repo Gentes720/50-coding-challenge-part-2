2eff1e9947a954ba869a77f57f3cf28474a3462e
Gentes720 <lontsiangelin@gmail.com>
2024-12-11 21:56:44 +0100

    Ex 36 from string to an array of caracters

---
+++ 036.py
@@ -0,0 +1,6 @@
+import numpy as np
+def sting_to_character(string):
+    array_of_caracters = np.array(list(string), dtype = object)
+    return(array_of_caracters)
+
+print(sting_to_character("jewojlreljej"))
